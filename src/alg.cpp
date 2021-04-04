4010e380d125451574bf72b73a688e95b4272b3e
Gornovaya <79137339+Gornovaya@users.noreply.github.com>
2021-04-04 23:33:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,23 +12,23 @@ int countPairs1(int* arr, int len, int value) {
 }
 
 int countPairs2(int* arr, int len, int value) {
-  int left = 0; 
-  int k = 0; 
+  int left = 0;
+  int k = 0;
   int right = len - 1;
 
   while (left < right - 1) {
-   int mid = (left + right)/2;
+    int mid = (left + right) / 2;
     if (value >= arr[mid])
       left = mid;
-    else right = mid;
-    }
-  
-len = right - 1;
+    else
+      right = mid;
+  }
 
-  for (int i = len; i>= 0; i--) {
+  len = right - 1;
+
+  for (int i = len; i >= 0; i--) {
     for (int j = 0; j < i; j++) {
-      if (arr[i] + arr[j] == value)
-       k = k + 1;
+      if (arr[i] + arr[j] == value) k = k + 1;
     }
   }
   return k;
